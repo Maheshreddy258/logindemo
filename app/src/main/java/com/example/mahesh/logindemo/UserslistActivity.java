c661a8af1eb8e9d217370ffcea9508d0682aeae8
mahesh <maheshreddy258.m2@gmail.com>
2019-02-02 14:53:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mahesh/logindemo/UserslistActivity.java
@@ -0,0 +1,99 @@
+package com.example.mahesh.logindemo;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.mahesh.logindemo.DB.DatabaseHelper;
+import com.example.mahesh.logindemo.adapters.UsersRecyclerAdapter;
+import com.example.mahesh.logindemo.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserslistActivity extends AppCompatActivity {
+
+    private AppCompatActivity activity = UserslistActivity.this;
+    private AppCompatTextView textViewName;
+    private RecyclerView recyclerViewUsers;
+    private AppCompatButton appCompatButtonLogout;
+    private List<User> listUsers;
+    private UsersRecyclerAdapter usersRecyclerAdapter;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userslist);
+        getSupportActionBar().setTitle("");
+        initViews();
+        initObjects();
+
+    }
+
+    /**
+     * This method is to initialize views
+     */
+    private void initViews() {
+        textViewName = (AppCompatTextView) findViewById(R.id.textViewName);
+        recyclerViewUsers = (RecyclerView) findViewById(R.id.recyclerViewUsers);
+        appCompatButtonLogout = (AppCompatButton) findViewById(R.id.appCompatButtonLogout);
+        appCompatButtonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserslistActivity.this,LoginActivity.class);
+            }
+        });
+    }
+
+
+    /**
+     * This method is to initialize objects to be used
+     */
+    private void initObjects() {
+        listUsers = new ArrayList<>();
+        usersRecyclerAdapter = new UsersRecyclerAdapter(listUsers);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerViewUsers.setLayoutManager(mLayoutManager);
+        recyclerViewUsers.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewUsers.setHasFixedSize(true);
+        recyclerViewUsers.setAdapter(usersRecyclerAdapter);
+        databaseHelper = new DatabaseHelper(activity);
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+        textViewName.setText(emailFromIntent);
+
+        getDataFromSQLite();
+    }
+
+    /**
+     * This method is to fetch all user records from SQLite
+     */
+    private void getDataFromSQLite() {
+        // AsyncTask is used that SQLite operation not blocks the UI Thread.
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                listUsers.clear();
+                listUsers.addAll(databaseHelper.getAllUser());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                usersRecyclerAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+}
